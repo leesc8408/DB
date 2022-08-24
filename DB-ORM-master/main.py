5ab968ae30b7a9e553973ce0fe1fd2188bb9148f
leesc8408 <leesc8408@gmail.com>
2022-08-25 08:48:43 +0900

    0825_08:48

--- DB-ORM-master/main.py
+++ DB-ORM-master/main.py
@@ -19,3 +19,41 @@ artist = Artist()
 artist.name = '아이유'
 artist.debut = '2019-12-26'
 artist.save()
+
+Director.objects.create(
+    name = '봉준호',
+    debut = '1993-01-01',
+    country = 'KOR'
+)
+
+# 기존에 5번까지는 생성한 이력이 있어서 id를 별도로 지정
+Genre.objects.create(id = 5, title = '스릴러')
+# id=5번은 기존 값이 삭제 안되어 있어서 값을 불러서 수정
+genre = Genre.objects.get(id = 5)
+genre.title = '스릴러'
+genre.save()
+# id번호 6번부터는 title만 지정하면 id는 자동으로
+Genre.objects.create(title = '재난')
+
+for i in Director.objects.all():
+    print(i.name, i.debut, i.country)
+
+direct = Director.objects.get(id = 1)
+print(direct.name, direct.debut, direct.country)
+
+direct = Director.objects.get(country = 'USA')
+print(direct.name, direct.debut, direct.country)
+
+direct = Director.objects.get(name = 'Joseph Kosinski')
+direct.country = 'USA'
+direct.save()
+print(direct.name, direct.debut, direct.country)
+
+direct = Director.objects.get(country = 'KOR')
+print(direct.name, direct.debut, direct.country)
+
+for direct in Director.objects.filter(country = 'KOR'):
+    print(direct.name, direct.debut, direct.country)
+
+direct = Director.objects.get(name = '김철수')
+direct.delete()
